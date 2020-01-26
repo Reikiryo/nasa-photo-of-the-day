c060e2eed67aea51d7c19186660436b55e9f0026
Josiah <reikiryo@yahoo.com>
2020-01-26 17:10:43 -0500

    adds button to call api based on input date

--- src/components/date.js
+++ src/components/date.js
@@ -1,18 +1,17 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const MainDiv = styled.div`
     height: 80%;
     maxwidth: 1000px;
     margin: 0 auto;
-    border: 1px solid white;
 `
 const ImageDiv = styled.div`
     margin: 2% auto;
     padding: 2%;
     background: white;
     width: 30.3%;
-    border: 2px solid white;
 `
 const DateImg = styled.img`
     width: 100%;
@@ -29,29 +28,50 @@ const MoreInfo = styled.div`
     font-size: 1rem;
     bottom: 0;
 `
-
 const DateTextDiv = styled.div`
     text-align: center;
     width: 20%;
     margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
 const DateText = styled.textarea`
     resize: none;
 `
-// const text1Value = document.getElementById('text1').value
-// const text2Value = document.getElementById('text2').value
-// const text3Value = document.getElementById('text3').value
-
-const textCallback = (e) => {
-    const value = e.target.value
-    console.log(value)
-    
-}
+const DateButton = styled.button`
+    width: 20%:
+    background-color: white;
+`
 
 export default function DropDown(props) {
     const [text1, setText1] = useState('2012')
     const [text2, setText2] = useState('03')
     const [text3, setText3] = useState('14')
+
+    const date = `${text1}-${text2}-${text3}`
+    const api = 'https://api.nasa.gov/planetary/apod?api_key=Fnat6NGYaQR1xNu6JUJYNWenhXMF3mmiBjvxyier&date=' + date
+
+    console.log(api)
+    const callapi = (api) => {
+        axios.get(api)
+        .then(res => {
+        console.log(res) 
+        })
+    } 
+    
+    const textCallback = (e) => {
+        const value = e.target.value
+        const id = e.target.id
+        console.log(value)
+        if (id === 'text1') {
+            setText1(value)
+        } else if (id === 'text2') {
+            setText2(value)
+        } else if (id === 'text3') {
+            setText3(value)
+        }
+    }
 
     return (
         <MainDiv>
@@ -60,6 +80,7 @@ export default function DropDown(props) {
             <DateText onChange={e => textCallback(e)} id="text1" key="1" rows="1" cols="4">2012</DateText>
             <DateText onChange={e => textCallback(e)} id="text2" key="2" rows="1" cols="2">03</DateText>
             <DateText onChange={e => textCallback(e)} id="text3" key="3" rows="1" cols="2">14</DateText>
+            <DateButton onClick={() => console.log('working')}>Go!</DateButton>
             </DateTextDiv>
             <ImageDiv>
                 <DateImg src={props.image} />
